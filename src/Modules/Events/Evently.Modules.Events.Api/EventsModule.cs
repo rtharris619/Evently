bb929d2238d9da9aa036ed97a4971ca3ece7c1dd
Ryan Harris <ryan@synthesis.co.za>
2025-01-20 16:40:47 +0200

    Add project files.

---
+++ src/Modules/Events/Evently.Modules.Events.Api/EventsModule.cs
@@ -0,0 +1,37 @@
+﻿
+using Evently.Modules.Events.Api.Database;
+using Evently.Modules.Events.Api.Events;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Evently.Modules.Events.Api;
+
+public static class EventsModule
+{
+    public static void MapEndpoints(IEndpointRouteBuilder routeBuilder)
+    {
+        CreateEvent.MapEndPoint(routeBuilder);
+        GetEvent.MapEndpoint(routeBuilder);
+    }
+
+    public static IServiceCollection AddEventsModule(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        string dbConnectionString = configuration.GetConnectionString("Database");
+
+        services.AddDbContext<EventsDbContext>(options =>
+            options
+                .UseNpgsql(
+                    dbConnectionString,
+                    npgsqlOptions => npgsqlOptions
+                        .MigrationsHistoryTable(HistoryRepository.DefaultTableName, Schemas.Events))
+                .UseSnakeCaseNamingConvention()
+        );
+
+        return services;
+    }
+}
