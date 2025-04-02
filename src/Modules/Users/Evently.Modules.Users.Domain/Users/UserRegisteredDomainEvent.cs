2bd5d1d9d4776b753e4af311462e784ffaabf043
Ryan Harris <ryan@synthesis.co.za>
2025-04-02 16:04:43 +0200

    Started scaffolding user module

---
+++ src/Modules/Users/Evently.Modules.Users.Domain/Users/UserRegisteredDomainEvent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Evently.Common.Domain;
+
+namespace Evently.Modules.Users.Domain.Users;
+
+public sealed class UserRegisteredDomainEvent(Guid userId) : DomainEvent
+{
+    public Guid UserId { get; init; } = userId;
+}
