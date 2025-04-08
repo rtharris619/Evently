55d5408d085c62858acd1082c4bc8494c5b2e459
Ryan Harris <ryan@synthesis.co.za>
2025-04-08 17:17:59 +0200

    Added more ticketing use cases and some endpoints

---
+++ src/Modules/Ticketing/Evently.Modules.Ticketing.Application/Orders/GetOrders/GetOrdersQueryHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Data.Common;
+using Dapper;
+using Evently.Common.Application.Data;
+using Evently.Common.Application.Messaging;
+using Evently.Common.Domain;
+
+namespace Evently.Modules.Ticketing.Application.Orders.GetOrders;
+
+internal sealed class GetOrdersQueryHandler(IDbConnectionFactory dbConnectionFactory)
+    : IQueryHandler<GetOrdersQuery, IReadOnlyCollection<OrdersResponse>>
+{
+    public async Task<Result<IReadOnlyCollection<OrdersResponse>>> Handle(
+        GetOrdersQuery request,
+        CancellationToken cancellationToken)
+    {
+        await using DbConnection connection = await dbConnectionFactory.OpenConnectionAsync();
+
+        const string sql =
+            $"""
+             SELECT
+                 id AS {nameof(OrdersResponse.Id)},
+                 customer_id AS {nameof(OrdersResponse.CustomerId)},
+                 status AS {nameof(OrdersResponse.Status)},
+                 total_price AS {nameof(OrdersResponse.TotalPrice)},
+                 created_at_utc AS {nameof(OrdersResponse.CreatedAtUtc)}
+             FROM ticketing.orders
+             WHERE customer_id = @CustomerId
+             """;
+
+        List<OrdersResponse> orders = (await connection.QueryAsync<OrdersResponse>(sql, request)).AsList();
+
+        return orders;
+    }
+}
---
+++ src/Modules/Ticketing/Evently.Modules.Ticketing.Presentation/Orders/GetOrder.cs
@@ -0,0 +1,24 @@
+﻿using Evently.Common.Domain;
+using Evently.Common.Presentation.Endpoints;
+using Evently.Common.Presentation.ApiResults;
+using Evently.Modules.Ticketing.Application.Orders.GetOrder;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace Evently.Modules.Ticketing.Presentation.Orders;
+
+internal sealed class GetOrder : IEndPoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder routeBuilder)
+    {
+        routeBuilder.MapGet("orders/{id}", async (Guid id, ISender sender) =>
+        {
+            Result<OrderResponse> result = await sender.Send(new GetOrderQuery(id));
+
+            return result.Match(Results.Ok, ApiResults.Problem);
+        })
+        .WithTags(Tags.Orders);
+    }
+}
