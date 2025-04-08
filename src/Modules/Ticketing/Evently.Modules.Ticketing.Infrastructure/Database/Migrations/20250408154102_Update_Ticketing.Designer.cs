a81dfba83c82f3d12ae37e50e829c83b6709aaa2
Ryan Harris <ryan@synthesis.co.za>
2025-04-08 18:14:32 +0200

    Added Keycloak IDP.

--- src/Modules/Ticketing/Evently.Modules.Ticketing.Infrastructure/Database/Migrations/20250408154102_Update_Ticketing.Designer.cs
+++ src/Modules/Ticketing/Evently.Modules.Ticketing.Infrastructure/Database/Migrations/20250408154102_Update_Ticketing.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Evently.Modules.Ticketing.Infrastructure.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Evently.Modules.Ticketing.Infrastructure.Database.Migrations
 {
     [DbContext(typeof(TicketingDbContext))]
-    partial class TicketingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250408154102_Update_Ticketing")]
+    partial class Update_Ticketing
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
