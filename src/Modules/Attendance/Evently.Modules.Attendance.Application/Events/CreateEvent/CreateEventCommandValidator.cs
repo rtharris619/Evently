9d3b7e103207bb037aac5678ad34fdde3e667425
Ryan Harris <ryan@synthesis.co.za>
2025-04-22 10:29:15 +0200

    Attendee module boilerplate and setup.

---
+++ src/Modules/Attendance/Evently.Modules.Attendance.Application/Events/CreateEvent/CreateEventCommandValidator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+
+namespace Evently.Modules.Attendance.Application.Events.CreateEvent;
+
+internal sealed class CreateEventCommandValidator : AbstractValidator<CreateEventCommand>
+{
+    public CreateEventCommandValidator()
+    {
+        RuleFor(c => c.EventId).NotEmpty();
+        RuleFor(c => c.Title).NotEmpty();
+        RuleFor(c => c.Description).NotEmpty();
+        RuleFor(c => c.Location).NotEmpty();
+        RuleFor(c => c.StartsAtUtc).NotEmpty();
+        RuleFor(c => c.EndsAtUtc).Must((cmd, endsAt) => endsAt > cmd.StartsAtUtc).When(c => c.EndsAtUtc.HasValue);
+    }
+}
---
+++ src/Modules/Attendance/Evently.Modules.Attendance.Domain/Attendees/AttendeeErrors.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Evently.Common.Domain;
+
+namespace Evently.Modules.Attendance.Domain.Attendees;
+
+public static class AttendeeErrors
+{
+    public static Error NotFound(Guid attendeeId) =>
+        Error.NotFound("Attendees.NotFound", $"The attendee with the identifier {attendeeId} was not found");
+}
